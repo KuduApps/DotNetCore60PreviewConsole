75eec5cacfebf82e4b976cc1a91ef0e825e0cc13
Purva Vasudeo <puvasude@microsoft.com>
2021-04-12 18:09:59 -0700

    Initial commit

---
+++ TestDotnet6ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace TestDotnet6ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Running {typeof(string).Assembly.Location}");
+            Thread.Sleep(10000);
+        }
+    }
+}
